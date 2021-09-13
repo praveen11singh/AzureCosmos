40ce2d8fbe36da0de80e338c740215e6a122b23d
praveen11singh <praveen11_singh11@outlook.com>
2021-09-13 16:33:43 +0000

    First commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+namespace ConsoleApp7
+{
+    class JsonData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             const string EndpointUrl = @"https://pkcosmosaccount.documents.azure.com:443/";
+             const string PrimaryKey = @"BTqe4xYQulPLMOVmZjl71L5gM3jwgO2ykSuewT1LjYIMKuyqL6tJbiPdmvyl39oMVlraUToXjggIfGn8GteU3A==";
+             DocumentClient client;
+             client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
+
+            IQueryable<JsonData> familyQuery = client.CreateDocumentQuery<JsonData>(
+                    UriFactory.CreateDocumentCollectionUri("praveen", "container1"), queryOptions);
+            foreach (var x in familyQuery)
+            {
+                Console.WriteLine("\tRead {0}", x.id + "  " + x.name);
+            }
+            Console.Read();
+        }
+    }
+}
